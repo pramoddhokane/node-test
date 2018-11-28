05ead036a1e5c873609bbe4e9830d5445f64ccfe
Pramod Dhokane <Pramod.Dhokane>
2018-11-28 14:07:35 +0530

    test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var http = require('http');
+var fs = require("fs");
 
 app.use(express.json());
 
@@ -7,7 +9,11 @@ const names = [{ id: 1, fname: "CPP" }, { id: 2, fname: "Java" }];
 var port = process.env.PORT || 8080;
 
 app.get("/", function(req, res) {
-  res.send(names);
+  fs.readFile("index.html", function(err, data){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
+  res.end();
+});
 });
 
 app.get("/getcourses/:id", function(req, res) {
